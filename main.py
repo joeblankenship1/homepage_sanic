6750a324ea92382f95a649e9df3e56a3b3b59e22
Joe Blankenship <thejoeblankenship@protonmail.com>
2018-12-30 20:16:26 -0500

    better structure to templates, added docstring

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ from sanic import Sanic
 from sanic.response import json, html, text
 from jinja2 import Environment, PackageLoader, select_autoescape
 
+# define the environment for the Jinja2 templates
 env = Environment(
     loader=PackageLoader('main', 'templates'),
     autoescape=select_autoescape(['html', 'xml', 'tpl'])
 )
 
 
+# a function for loading an HTML template from the Jinja environment
 def template(tpl, **kwargs):
     template = env.get_template(tpl)
     return html(template.render(kwargs))
 
 
+# create the Sanic app and serve it statically
 app = Sanic(__name__)
 app.static('/static', './static')
 
 
+# define our function for our homepage
 @app.route('/')
 async def home(request):
     greeting = 'Hello, Sanic!'
@@ -29,5 +33,6 @@ async def home(request):
     )
 
 
+# run the main.py on http://localhost:8000
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000)
